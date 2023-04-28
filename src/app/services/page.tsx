5aad846f9a88b7675751097b8c4353a078a6d7a6
Artur Blanco <arturblancop@gmail.com>
2023-04-28 18:02:41 -0300

    first commit

---
+++ src/app/services/page.tsx
@@ -0,0 +1,25 @@
+export default function Services() {
+  return (
+    <div>
+      <ul>
+        <li className="py-2">
+          <p>Serviços bancários em geral;</p>
+          <p>Coleta de assinaturas em contratos de investimentos;</p>
+        </li>
+        <li className="py-2">
+          <p>Serviços em cártorios, comercias e jurídicos;;</p>
+          <p>Receita Federal;</p>
+          <p>Cobraças em geral;</p>
+        </li>
+        <li className="py-2">
+          <p>Entrega de brindes e convites;</p>
+          <p>Remessas de correspondências junto aos correios;</p>
+        </li>
+        <li className="py-2">
+          <p>Despachos em aeroportos e transportadoras;</p>
+          <p>Serviços de malotes entre filiais;</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
